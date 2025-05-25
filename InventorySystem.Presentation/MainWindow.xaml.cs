c793ae21832ae7717eb426f2010745ad124a9f82
AHorrificNecktie <147227094+AHorrificNecktie@users.noreply.github.com>
2025-05-25 23:33:42 +0200

    Using WPF

---
+++ InventorySystem.Presentation/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+
+namespace InventorySystem.Presentation
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenInventory_Click(object sender, RoutedEventArgs e)
+        {
+            new InventoryOverviewWindow().ShowDialog();
+        }
+
+        private void OpenShipments_Click(object sender, RoutedEventArgs e)
+        {
+            new ShipmentOverviewWindow().ShowDialog();
+        }
+
+        private void OpenWarehouses_Click(object sender, RoutedEventArgs e)
+        {
+            new WarehouseWindow().ShowDialog();
+        }
+
+        private void OpenTrucks_Click(object sender, RoutedEventArgs e)
+        {
+            new TruckWindow().ShowDialog();
+        }
+
+        private void OpenMap_Click(object sender, RoutedEventArgs e)
+        {
+            new WarehouseMapWindow().ShowDialog();
+        }
+    }
+}
